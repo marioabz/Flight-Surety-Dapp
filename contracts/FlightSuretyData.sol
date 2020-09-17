34c747371d66247fa6fa7480be47b6d7b06a3a64
Mario Briseño <mario.briseno7@gmail.com>
2020-09-16 23:56:09 -0500

    First set of modifications to Data contract

--- contracts/FlightSuretyData.sol
+++ contracts/FlightSuretyData.sol
@@ -3,84 +3,121 @@ pragma solidity ^0.4.25;
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract FlightSuretyData {
+    
     using SafeMath for uint256;
 
     address private contractOwner; // Account used to deploy contract
     bool private operational = true; // Blocks all state changes throughout the contract if false
 
-    /**
-     * @dev Constructor
-     *      The deploying account becomes contractOwner
-     */
+    struct Airline {
+        string name;
+        bool allowed;
+        bool canVote;
+        address airlineAddress;
+        uint8 position;
+    }
+
+    struct Votes {
+        address airline;
+        bool vote;
+    }
+    Votes[] votes;
+
+    uint8 noRegisteredAirlines;
+    uint8 approvedAirlines; //Votes by airlines
+
+    mapping(address => Airline) public airlines;
+
+    struct Flight {
+        bool isRegistered;
+        uint8 statusCode;
+        uint256 updatedTimestamp;        
+        address airline;
+    }
+    mapping(bytes32 => Flight) private flights;
+    mapping (address => bool) authAddresses;
+    mapping(address => uint) debtUsers;
+    
+    
     constructor() public {
         contractOwner = msg.sender;
+        approvedAirlines = 0;
+        noRegisteredAirlines = 0;
     }
-    /**
-     * @dev Modifier that requires the "operational" boolean variable to be "true"
-     *      This is used on all state changing functions to pause the contract in
-     *      the event there is an issue that needs to be fixed
-     */
+
+    // Modifier that requires the "operational" boolean variable to be "true"
     modifier requireIsOperational() {
         require(operational, "Contract is currently not operational");
         _; 
     }
 
-    /**
-     * @dev Modifier that requires the "ContractOwner" account to be the function caller
-     */
     modifier requireContractOwner() {
         require(msg.sender == contractOwner, "Caller is not contract owner");
         _;
     }
 
-    /**
-     * @dev Get operating status of contract
-     * @return A bool that is the current operating status
-     */
+    modifier requireFirstAirlineApproval() {
+        if(noRegisteredAirlines > 0 && noRegisteredAirlines < 4) {
+            require(airlines[msg.sender].position == 1);
+        }
+        _;
+    }
 
+    modifier airlineIsAllowed() {
+        require(airlines[msg.sender].allowed, "This airline is not allowed to vote");
+        _;
+    }
+
+    //Get operating status of contract
     function isOperational() public view returns (bool) {
         return operational;
     }
 
-    /**
-     * @dev Sets contract operations on/off
-     * When operational mode is disabled, all write transactions except for this one will fail
-     */
-
+    // When operational mode is disabled, all write transactions except for this one will fail
     function setOperatingStatus(bool mode) external requireContractOwner {
         operational = mode;
+    }
+
+    function _registerAirline(string memory _name) internal pure {
+        noRegisteredAirlines++;
+        airlines[_airline] = Airline(_name, false, false, msg.sender, noRegisteredAirlines);
+    }
+
+    //Add an airline to the registration queue. Can only be called from FlightSuretyApp contract
+    function registerAirline(string calldata _name) external pure{
+        
+        if(noRegisteredAirlines > 0 && noRegisteredAirlines < 4) {
+            require(airlines[msg.sender].position == 1);
+        }
+        uint8 requiredApprovals = noRegisteredAirlines / 2;
+        if(votes.length >= requiredApprovals) {
+            cleanVotes();
+            delete votes;
+            _registerAirline(_name);
+        }
 
     }
 
-    /**
-     * @dev Add an airline to the registration queue
-     *      Can only be called from FlightSuretyApp contract
-     */
-
-    function registerAirline() external pure {
-
+    function cleanVotes() internal pure {
+        for(uint i; i < votes.length;  i++) {
+            airlines[votes[i].airline].canVote = true;
+        }
     }
 
-    /**
-     * @dev Buy insurance for a flight
-     */
+    function voteAirline() external pure {
+    }
 
+    //uy insurance for a flight
     function buy() external payable {}
 
-    /**
-     *  @dev Credits payouts to insurees
-     */
+    //Credits payouts to insurees
     function creditInsurees() external pure {}
 
-    /**
-     *  @dev Transfers eligible payout funds to insuree
-     */
+    // Transfers eligible payout funds to insuree
     function pay() external pure {}
 
-    /**
-     * @dev Initial funding for the insurance. Unless there are too many delayed flights
-     *      resulting in insurance payouts, the contract should be self-sustaining
-     */
+    // Initial funding for the insurance. Unless there are too many delayed flights
+    // resulting in insurance payouts, the contract should be self-sustaining
 
     function fund() public payable {}
 
@@ -92,9 +129,7 @@ contract FlightSuretyData {
         return keccak256(abi.encodePacked(airline, flight, timestamp));
     }
 
-    /**
-     * @dev Fallback function for funding smart contract.
-     */
+    //Fallback function for funding smart contract.
     function() external payable {
         fund();
     }
