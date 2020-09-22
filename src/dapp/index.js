229b1a738f0708cb154d1e785325f0215ab7957e
Mario Briseño <mario.briseno7@gmail.com>
2020-09-22 17:41:17 -0500

    Adding function to enter data to input

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -18,6 +18,12 @@ let statusCodeProb = [0,20,20,20,0,20,20,20,20,0];
             console.log(error,result);
             display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
         });
+
+        let flightsDisplayer = DOM.elid("flights-displayer");
+        flightsDisplayer.appendChild(DOM.div({className: "flight-title"}, "Scheduled Flights"));
+        flights.map(flight => {
+        flightsDisplayer.appendChild(DOM.div({className: 'flight'}, flight));
+        })
     
         // User-submitted transaction
         DOM.elid('submit-oracle').addEventListener('click', () => {
@@ -27,9 +33,12 @@ let statusCodeProb = [0,20,20,20,0,20,20,20,20,0];
                 display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
             });
         })
+
+        DOM.elid('flights-displayer').addEventListener('click', (res) => {
+            DOM.elid('flight-number').value = res.toElement.innerText;
+        })
     });
     
-
 })();
 
 function getRandomInt(max) {
@@ -38,33 +47,16 @@ function getRandomInt(max) {
 
 function display(title, description, results) {
 
-    let _flights = [...Array(3)].map((_, i) => {
-        return {
-            time : new Date((new Date()).getTime() + getRandomInt(10)*9000000).toLocaleString(),
-            airline : flights[getRandomInt(flights.length)],
-            code : getRandomInt(7)*10,
-            registered : getRandomInt(2) === 1
-        }
-    })
-
-    console.log(_flights);
-
     let displayDiv = DOM.elid("display-wrapper");
     let section = DOM.section();
     section.appendChild(DOM.h2(title));
     section.appendChild(DOM.h5(description));
     results.map((result) => {
-        let row = section.appendChild(DOM.div({className:'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
+        let row = section.appendChild(DOM.div({className: 'key-field'}));
+        row.appendChild(DOM.div({className: 'key'}, result.label + ":"));
+        row.appendChild(DOM.div({className: 'field'}, result.error ? String(result.error) : String(result.value)));
         section.appendChild(row);
     })
-
-    let flightsDisplayer = DOM.elid("flights-displayer");
-    flights.map(flight => {
-        flightsDisplayer.appendChild(DOM.div(flight));
-    })
-
     displayDiv.append(section);
 
 }
